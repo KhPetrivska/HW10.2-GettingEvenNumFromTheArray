dc010e252a8929a7c823f532bd761fe47708feb9
Kristina <kristin.petrivska@gmail.com>
2024-08-06 12:50:48 -0600

    remove slice

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 //debugger
 const array = [1, 2, 3, 4, 12, 0,3, -2, 8];
-const evenNumArray = array.slice().filter(element => {
+const evenNumArray = array.filter(element => {
    return Number.isInteger(element / 2);
 });
 
