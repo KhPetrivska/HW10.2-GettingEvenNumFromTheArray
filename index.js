c3b23a61360a16fb58683946418e9f81ef6ed724
Kristina <kristin.petrivska@gmail.com>
2024-08-05 20:28:46 -0600

    add the code in html and js files

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+//debugger
+const array = [1, 2, 3, 4, 12, 0,3, -2, 8];
+const evenNumArray = array.slice().filter(element => {
+   return Number.isInteger(element / 2);
+});
+
+console.log(evenNumArray);
